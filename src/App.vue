dfda25d44f949b3c80dc4beda833ff32d46ac2db
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-04-21 17:12:51 +0100

    Load Product Data

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container fluid id="app">
+    <ProductDataLoader v-on:change="productData = $event" />
+    <div v-if="productData">
+      {{productData.description}}
+    </div>
+  </b-container>
+</template>
+
+<script>
+import ProductDataLoader from './components/ProductDataLoader'
+
+export default {
+  name: 'App',
+  data: () => ({
+    productData: null
+  }),
+  components: {
+    ProductDataLoader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/ProductDataLoader.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-row>
+    <b-col>
+      <h1>
+        GIF for your Product
+      </h1>
+      <p>
+        Enter your product url to create an animated gif from its images
+      </p>
+      <b-form @submit.prevent="onSubmit">
+        <b-form-group description="Example: https://colourpop.com/products/yes-please">
+          <b-input-group>
+            <b-form-input id="productUrl"
+                          name="productUrl"
+                          type="url"
+                          v-model="productUrl"
+                          required
+                          placeholder=""></b-form-input>
+            <b-input-group-append>
+              <b-btn type="submit" variant="primary">Preview</b-btn>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-form>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'ProductDataLoader',
+  data: () => ({
+    productUrl: null
+  }),
+  methods: {
+    async onSubmit () {
+      const { productUrl } = this
+      if (productUrl && productUrl.length) {
+        const data = await fetch(productUrl + '.js', { mode: 'cors' })
+          .then(response => response.json())
+          // .catch(error => null)
+        if (data) {
+          this.$emit('change', data)
+          this.productUrl = null
+        }
+      }
+    }
+  }
+}
+</script>
